986e5717287780e5e6cb9cc839a66f7ebcaaee0f
Ramakandra <ramakandra@protonmail.com>
2017-09-13 19:05:26 +0000

    book

---
+++ notes/configure/gitbook.sh
@@ -0,0 +1,7 @@
+curl -sL https://deb.nodesource.com/setup_6.x -o nodesource_setup.sh
+sudo bash nodesource_setup.sh
+sudo apt-get install nodejs
+sudo apt-get install build-essential
+npm install gitbook-cli -g
+gitbook init
+gitbook serve
