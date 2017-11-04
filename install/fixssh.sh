e0dd14ed15007b87bcebd8844c0104c44b9acd9b
Maurice Steenbergen <m@poing.com>
2017-11-04 09:43:58 +0100

    update

--- install/fixssh.sh
+++ install/fixssh.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+# install openssh-server
+apt-get install openssh-server
+
+# remove current runlevels for SSH
+update-rc.d -f ssh remove
+
+# load default SSH runlevels
+update-rc.d -f ssh defaults
+
+# move old keys
+mkdir /etc/ssh/insecure_original_keys
+mv /etc/ssh/ssh_host_* insecure_original_keys
+
+# create new keys
+dpkg-reconfigure openssh-server
